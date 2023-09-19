c6fbc82e6b4cfcfddc2bb97d1947515414755035
leong <leon.goergen@gmail.com>
2023-09-19 17:21:41 +0200

    fixed decoding bug

--- src/app/game-overview/services/rle.service.ts
+++ src/app/game-overview/services/rle.service.ts
@@ -7,54 +7,98 @@ import {GRID_CONSTANTS} from "../../app.constants";
   providedIn: 'root'
 })
 export class RleService {
+  private x!: number;
+  private y!: number;
+  private step!: number;
+  private cells!: Cell[];
+  private isIgnoring!: boolean;
 
   public rleLoaded$ = new Subject<Cell[]>();
 
-  decodeRLE(rleString: string, startX: number, startY: number): void {
-    if (/[^bo$0-9!]/.test(rleString)) {
+  public decodeRLE(rleString: string, startX: number, startY: number): void {
+    if (/[^bo$0-9!#x]/.test(rleString)) {
       console.error('Invalid characters in input string.');
       return;
     }
 
-    const lines = rleString.slice(0, -1).split('$');
-    const cells: Cell[] = [];
+    this.initVariables();
 
-    lines.forEach((line, y) => {
-      const expandedLine = this.expandLine(line);
-
-      for (let x = 0; x < expandedLine.length; x++) {
-        cells.push(
-          new Cell(
-            startX + x * GRID_CONSTANTS.CELL_SIZE,
-            startY + y * GRID_CONSTANTS.CELL_SIZE,
-            expandedLine[x] === 'o'
-          )
-        );
+    for (let i = 0; i < rleString.length; i++) {
+      if (this.isIgnoring) {
+        if (rleString[i] === "\n") {
+          this.isIgnoring = false;
+        }
+        continue;
       }
-    });
 
-    this.rleLoaded$.next(cells);
-  }
-
-  private expandLine(line: string): string {
-    let expandedLine = '';
-    let i = 0;
-
-    while (i < line.length) {
-      let repeatCount = '';
-      while (i < line.length && !isNaN(Number(line[i]))) {
-        repeatCount += line[i];
-        i++;
+      switch (rleString[i]) {
+        case "#":
+        case "x":
+        case "!":
+          this.isIgnoring = true;
+          continue;
+        case "o":
+          this.updateCoordinatesForO(startX, startY);
+          continue;
+        case "b":
+          this.updateCoordinatesForB();
+          continue;
+        case "$":
+          this.updateCoordinatesForDollar();
+          continue;
+        default:
+          i = this.updateStep(i, rleString);
       }
-      if (repeatCount && i < line.length) {
-        expandedLine += line[i].repeat(Number(repeatCount));
-      } else if (i < line.length) {
-        expandedLine += line[i];
-      }
-      i++;
     }
 
-    return expandedLine;
+    this.rleLoaded$.next(this.cells);
+  }
+
+  private resetStep = () => {
+    this.step = 1;
+  };
+
+  private updateCoordinatesForO(startX: number, startY: number) {
+    for (let j = 0; j < this.step; j++) {
+      this.cells.push(
+        new Cell(
+          startX + this.x * GRID_CONSTANTS.CELL_SIZE,
+          startY + this.y * GRID_CONSTANTS.CELL_SIZE,
+          true
+        )
+      );
+      this.x++;
+    }
+    this.resetStep();
+  };
+
+  private updateCoordinatesForB() {
+    this.x += this.step;
+    this.resetStep();
+  };
+
+  private updateCoordinatesForDollar() {
+    this.x = 0;
+    this.y += this.step;
+    this.resetStep();
+  };
+
+  private updateStep(i: number, rleString: string) {
+    const regex = /^\d+/;
+    const match = regex.exec(rleString.slice(i));
+    if (match) {
+      this.step = parseInt(match[0]);
+      i += match[0].length - 1;
+    }
+    return i;
+  };
+
+  private initVariables() {
+    this.x = 0;
+    this.y = 0;
+    this.step = 1;
+    this.cells = [];
+    this.isIgnoring = false;
   }
 
 }
