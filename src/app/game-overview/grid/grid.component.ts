c3b2d2c164cba6eed44decf2b4b429be5ac2d04c
leong <leon.goergen@gmail.com>
2023-09-20 19:29:30 +0200

    small refactor grid component

--- src/app/game-overview/grid/grid.component.ts
+++ src/app/game-overview/grid/grid.component.ts
@@ -12,8 +12,8 @@ import {RleService} from "../services/rle.service";
   styleUrls: ['./grid.component.css']
 })
 export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
-  CANVAS_WIDTH = GRID_CONSTANTS.CANVAS_WIDTH;
-  CANVAS_HEIGHT = GRID_CONSTANTS.CANVAS_HEIGHT;
+  CANVAS_WIDTH: number = GRID_CONSTANTS.CANVAS_WIDTH;
+  CANVAS_HEIGHT: number = GRID_CONSTANTS.CANVAS_HEIGHT;
   @ViewChild('gridCanvas', { static: true }) gridCanvas!: ElementRef<HTMLCanvasElement>;
   @ViewChild('gameCanvas', { static: true }) gameCanvas!: ElementRef<HTMLCanvasElement>;
 
@@ -47,7 +47,7 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
               private gameService: GameService,
               private rleService: RleService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.subscriptions.push(
       this.gameService.nextGeneration$.subscribe(() => this.onNextGeneration()),
       this.gameService.lastGeneration$.subscribe(() => this.onLastGeneration()),
@@ -69,7 +69,7 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     setInterval(() => this.updateFPSDisplay(), 100);
   }
 
-  initGrid(): void {
+  private initGrid(): void {
     this.cells.clear();
     this.cellsToCheck.clear();
     this.generationDeltas = [];
@@ -81,36 +81,36 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     this.panToMiddle();
   }
 
-  updateFPSDisplay(): void {
+  private updateFPSDisplay(): void {
     this.gameService.updateFps(this.frameCount * 10);
     this.frameCount = 0;
   }
 
   private panToMiddle(): void {
-    const middleX = this.gridSize / 2;
-    const middleY = this.gridSize / 2;
+    const middleX: number = this.gridSize / 2;
+    const middleY: number = this.gridSize / 2;
 
-    const canvasRect = this.gridCanvas.nativeElement.getBoundingClientRect();
-    const canvasMiddleX = canvasRect.width / 2;
-    const canvasMiddleY = canvasRect.height / 2;
+    const canvasRect: DOMRect = this.gridCanvas.nativeElement.getBoundingClientRect();
+    const canvasMiddleX: number = canvasRect.width / 2;
+    const canvasMiddleY: number = canvasRect.height / 2;
 
-    const translateX = canvasMiddleX - middleX;
-    const translateY = canvasMiddleY - middleY;
+    const translateX: number = canvasMiddleX - middleX;
+    const translateY: number = canvasMiddleY - middleY;
 
     this.transformationMatrixService.setTransform(1, 0, 0, 1, translateX, translateY);
 
-    requestAnimationFrame(() => {
+    requestAnimationFrame((): void => {
       this.drawGridLines();
       this.drawCells();
     });
   }
 
-  toggleGridLines(gridLines: boolean): void {
+  private toggleGridLines(gridLines: boolean): void {
     this.gridLines = gridLines;
     this.drawGridLines();
   }
 
-  drawGridLines(): void {
+  private drawGridLines(): void {
     this.gridCtx.clearRect(0, 0, this.gridSize, this.gridSize);
 
     if (!this.gridLines) {
@@ -121,9 +121,9 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.gridCtx.strokeStyle = GRID_COLORS.GRID_LINE;
 
-    const gridSpacing = this.getGridSpacing();
+    const gridSpacing: number = this.getGridSpacing();
 
-    for (let y = 0, i = 0; y <= this.gridSize; y += GRID_CONSTANTS.CELL_SIZE * gridSpacing, i += gridSpacing) {
+    for (let y: number = 0, i: number= 0; y <= this.gridSize; y += GRID_CONSTANTS.CELL_SIZE * gridSpacing, i += gridSpacing) {
       this.gridCtx.lineWidth = (i % 6 === 0) ? 2 : 0.5;
       this.gridCtx.beginPath();
       this.gridCtx.moveTo(0, y);
@@ -131,7 +131,7 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
       this.gridCtx.stroke();
     }
 
-    for (let x = 0, i = 0; x <= this.gridSize; x += GRID_CONSTANTS.CELL_SIZE * gridSpacing, i += gridSpacing) {
+    for (let x: number = 0, i: number = 0; x <= this.gridSize; x += GRID_CONSTANTS.CELL_SIZE * gridSpacing, i += gridSpacing) {
       this.gridCtx.lineWidth = (i % 6 === 0) ? 2 : 0.5;
       this.gridCtx.beginPath();
       this.gridCtx.moveTo(x, 0);
@@ -141,8 +141,8 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private getGridSpacing(): number {
-    const zoomLevel = this.transformationMatrixService.matrix[0];
-    let gridSpacing = 1;
+    const zoomLevel: number = this.transformationMatrixService.matrix[0];
+    let gridSpacing: number = 1;
 
     if (zoomLevel <= GRID_CONSTANTS.ZOOM_LEVEL_THRESHOLD && zoomLevel > GRID_CONSTANTS.ZOOM_LEVEL_THRESHOLD / 2) {
       gridSpacing = 2;
@@ -159,20 +159,20 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     return gridSpacing;
   }
 
-  initializeGridCells(): void {
-    const cellSize = GRID_CONSTANTS.CELL_SIZE;
-    const range = Array.from({length: this.gridSize / cellSize}, (_, i) => i * cellSize);
+  private initializeGridCells(): void {
+    const cellSize: number = GRID_CONSTANTS.CELL_SIZE;
+    const range: number[] = Array.from({length: this.gridSize / cellSize}, (_, i: number) => i * cellSize);
 
-    const cellPositions = range.flatMap(x => range.map(y => ({ x, y })));
+    const cellPositions: {x: number, y: number}[] = range.flatMap(x => range.map(y => ({ x, y })));
 
     cellPositions.forEach(({ x, y }) => {
-      const cell = new Cell(x, y, false);
+      const cell: Cell = new Cell(x, y, false);
       this.cells.set(cell.key, cell);
       if (cell.alive) { this.cellsToCheck.add(cell.key); }
     });
   }
 
-  drawCells(): void {
+  private drawCells(): void {
     const visibleGridRange = this.getVisibleGridRange();
 
     this.gameCtx.clearRect(0, 0, this.gridSize, this.gridSize);
@@ -183,29 +183,29 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.gameCtx.fillStyle = GRID_COLORS.ALIVE;
 
-    const cellsToCheck = this.getCellsToCheck(visibleGridRange);
+    const cellsToCheck: Cell[] = this.getCellsToCheck(visibleGridRange);
     cellsToCheck.forEach(cell => {
       if (cell.alive)
         this.gameCtx.fillRect(cell.x, cell.y, GRID_CONSTANTS.CELL_SIZE, GRID_CONSTANTS.CELL_SIZE);
     });
   }
 
-  getVisibleGridRange() {
-    const rect = this.gameCanvas.nativeElement.getBoundingClientRect();
+  private getVisibleGridRange(): {startCol: number, endCol: number, startRow: number, endRow: number}  {
+    const rect: DOMRect = this.gameCanvas.nativeElement.getBoundingClientRect();
 
-    const inverseMatrix = this.transformationMatrixService.inverseMatrix;
-    const topLeft = this.transformationMatrixService.transformPoint({x: 0, y: 0}, inverseMatrix);
-    const bottomRight = this.transformationMatrixService.transformPoint({x: rect.width, y: rect.height}, inverseMatrix);
+    const inverseMatrix: number[] = this.transformationMatrixService.inverseMatrix;
+    const topLeft: {x: number, y:number} = this.transformationMatrixService.transformPoint({x: 0, y: 0}, inverseMatrix);
+    const bottomRight: {x: number, y:number} = this.transformationMatrixService.transformPoint({x: rect.width, y: rect.height}, inverseMatrix);
 
-    const startCol = Math.floor(topLeft.x / GRID_CONSTANTS.CELL_SIZE);
-    const endCol = Math.ceil(bottomRight.x / GRID_CONSTANTS.CELL_SIZE);
-    const startRow = Math.floor(topLeft.y / GRID_CONSTANTS.CELL_SIZE);
-    const endRow = Math.ceil(bottomRight.y / GRID_CONSTANTS.CELL_SIZE);
+    const startCol: number = Math.floor(topLeft.x / GRID_CONSTANTS.CELL_SIZE);
+    const endCol: number = Math.ceil(bottomRight.x / GRID_CONSTANTS.CELL_SIZE);
+    const startRow: number = Math.floor(topLeft.y / GRID_CONSTANTS.CELL_SIZE);
+    const endRow: number = Math.ceil(bottomRight.y / GRID_CONSTANTS.CELL_SIZE);
 
     return {startCol, endCol, startRow, endRow};
   }
 
-  getCellsToCheck(visibleGridRange: any): Cell[] {
+  private getCellsToCheck(visibleGridRange: any): Cell[] {
     const keys: string[] = [];
 
     for (let x = visibleGridRange.startCol; x <= visibleGridRange.endCol; x++) {
@@ -220,18 +220,18 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
       .filter((cell): cell is Cell => cell !== undefined);
   }
 
-  startPan(event: MouseEvent): void {
+  protected startPan(event: MouseEvent): void {
     this.panConfig.isPanning = true;
     this.panConfig.lastPanX = event.clientX;
     this.panConfig.lastPanY = event.clientY;
     this.panConfig.totalPanDistance = 0;
   }
 
-  pan(event: MouseEvent): void {
+  protected pan(event: MouseEvent): void {
     if (!this.panConfig.isPanning) { return; }
 
-    const deltaX = event.clientX - this.panConfig.lastPanX;
-    const deltaY = event.clientY - this.panConfig.lastPanY;
+    const deltaX: number = event.clientX - this.panConfig.lastPanX;
+    const deltaY: number = event.clientY - this.panConfig.lastPanY;
 
     this.transformationMatrixService.translate(deltaX, deltaY, this.gridSize);
 
@@ -246,19 +246,19 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  endPan(): void {
+  protected endPan(): void {
     this.panConfig.isPanning = false;
   }
 
-  zoom(event: WheelEvent): void {
+  protected zoom(event: WheelEvent): void {
     event.preventDefault();
 
-    const rect = this.gridCanvas.nativeElement.getBoundingClientRect();
+    const rect: DOMRect = this.gridCanvas.nativeElement.getBoundingClientRect();
 
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+    const x: number = event.clientX - rect.left;
+    const y: number = event.clientY - rect.top;
 
-    const amount = 1 - event.deltaY * GRID_CONSTANTS.ZOOM_FACTOR;
+    const amount: number = 1 - event.deltaY * GRID_CONSTANTS.ZOOM_FACTOR;
     this.transformationMatrixService.scaleAt({ x, y }, amount, this.gridSize);
 
     requestAnimationFrame(() => {
@@ -267,11 +267,11 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  onCanvasClick(event: MouseEvent): void {
+  protected onCanvasClick(event: MouseEvent): void {
     if (this.panConfig.totalPanDistance > GRID_CONSTANTS.PAN_DISTANCE_THRESHOLD) { return; }
 
-    const key = this.getCellKeyByCoordinates(event);
-    const cell = this.cells.get(key);
+    const key: string = this.getCellKeyByCoordinates(event);
+    const cell: Cell | undefined = this.cells.get(key);
 
     if (!cell) { return; }
 
@@ -281,15 +281,15 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     this.drawCells();
   }
 
-  async onRightClick(event: MouseEvent): Promise<void> {
+  protected async onRightClick(event: MouseEvent): Promise<void> {
     event.preventDefault();
-    const key = this.getCellKeyByCoordinates(event);
-    const cell = this.cells.get(key)!;
-    const rleString = await navigator.clipboard.readText();
+    const key: string = this.getCellKeyByCoordinates(event);
+    const cell: Cell = this.cells.get(key)!;
+    const rleString: string = await navigator.clipboard.readText();
     this.rleService.decodeRLE(rleString, cell.x, cell.y);
   }
 
-  onCellsLoaded(cells: Cell[]): void {
+  private onCellsLoaded(cells: Cell[]): void {
     cells.forEach(cell => {
       this.cells.set(cell.key, cell);
       this.cellsToCheck.add(cell.key);
@@ -298,24 +298,24 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     this.drawCells();
   }
 
-  getCellKeyByCoordinates(event: MouseEvent) {
-    const rect = this.gameCanvas.nativeElement.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
+  private getCellKeyByCoordinates(event: MouseEvent): string {
+    const rect: DOMRect = this.gameCanvas.nativeElement.getBoundingClientRect();
+    const x: number = event.clientX - rect.left;
+    const y: number = event.clientY - rect.top;
 
-    const inverseMatrix = this.transformationMatrixService.inverseMatrix;
-    const worldPoint = this.transformationMatrixService.transformPoint({ x, y }, inverseMatrix);
+    const inverseMatrix: number[] = this.transformationMatrixService.inverseMatrix;
+    const worldPoint: {x: number, y:number} = this.transformationMatrixService.transformPoint({ x, y }, inverseMatrix);
 
-    const cellX = Math.floor(worldPoint.x / GRID_CONSTANTS.CELL_SIZE) * GRID_CONSTANTS.CELL_SIZE;
-    const cellY = Math.floor(worldPoint.y / GRID_CONSTANTS.CELL_SIZE) * GRID_CONSTANTS.CELL_SIZE;
+    const cellX: number = Math.floor(worldPoint.x / GRID_CONSTANTS.CELL_SIZE) * GRID_CONSTANTS.CELL_SIZE;
+    const cellY: number = Math.floor(worldPoint.y / GRID_CONSTANTS.CELL_SIZE) * GRID_CONSTANTS.CELL_SIZE;
     return `${cellX},${cellY}`;
   }
 
-  onNextGeneration(): void {
-    const newCellsToCheck = new Set<string>();
-    const aliveCells = new Map<string, boolean>();
+  private onNextGeneration(): void {
+    const newCellsToCheck: Set<string> = new Set<string>();
+    const aliveCells: Map<string, boolean> = new Map<string, boolean>();
 
-    const allCellsToCheck = this.getAllCellsToCheck();
+    const allCellsToCheck: Set<string> = this.getAllCellsToCheck();
     allCellsToCheck.forEach(key => this.determineNewCellState(key, aliveCells));
     this.convertToSet(aliveCells, newCellsToCheck);
 
@@ -323,7 +323,7 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
     if (this.generationDeltas.length > 500) { this.generationDeltas.shift(); }
 
     this.cellsToCheck = newCellsToCheck;
-    requestAnimationFrame(() => { this.drawCells(); });
+    requestAnimationFrame((): void => { this.drawCells(); });
 
     if (this.cellsToCheck.size > 0) { this.generationCount += 1; }
     this.frameCount += 1;
@@ -335,10 +335,10 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private getAllCellsToCheck(): Set<string> {
-    const allCellsToCheck = new Set<string>();
+    const allCellsToCheck: Set<string> = new Set<string>();
     this.cellsToCheck.forEach(key => {
       allCellsToCheck.add(key);
-      const cell = this.cells.get(key);
+      const cell: Cell | undefined = this.cells.get(key);
       if (cell) {
         this.getNeighborKeys(cell).forEach(neighborKey => allCellsToCheck.add(neighborKey));
       }
@@ -347,13 +347,13 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private determineNewCellState(key: string, aliveCells: Map<string, boolean>): void {
-    const cell = this.cells.get(key);
+    const cell: Cell | undefined = this.cells.get(key);
     if (!cell) { return; }
 
-    const aliveNeighbors = this.getNeighborKeys(cell)
-      .reduce((count, neighborKey) => count + (this.cells.get(neighborKey)?.alive ? 1 : 0), 0);
+    const aliveNeighbors: number = this.getNeighborKeys(cell)
+      .reduce((count: number, neighborKey: string) => count + (this.cells.get(neighborKey)?.alive ? 1 : 0), 0);
 
-    const shouldUpdate = cell.alive ? (aliveNeighbors < 2 || aliveNeighbors > 3) : (aliveNeighbors === 3);
+    const shouldUpdate: boolean = cell.alive ? (aliveNeighbors < 2 || aliveNeighbors > 3) : (aliveNeighbors === 3);
 
     if (shouldUpdate)
       aliveCells.set(key, !cell.alive);
@@ -362,8 +362,8 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private convertToSet(aliveCells: Map<string, boolean>, newCellsToCheck: Set<string>): void {
-    aliveCells.forEach((newState, key) => {
-      const cell = this.cells.get(key);
+    aliveCells.forEach((newState: boolean, key: string): void => {
+      const cell: Cell | undefined = this.cells.get(key);
       if (!cell) { return; }
 
       cell.alive = newState;
@@ -374,10 +374,10 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
   private onLastGeneration(): void {
     if (this.generationDeltas.length <= 0) { return; }
 
-    const lastGenerationDeltas = this.generationDeltas.pop()!;
+    const lastGenerationDeltas: Map<string, boolean> = this.generationDeltas.pop()!;
 
-    lastGenerationDeltas.forEach((newState, key) => {
-      const cell = this.cells.get(key);
+    lastGenerationDeltas.forEach((newState: boolean, key: string): void => {
+      const cell: Cell | undefined = this.cells.get(key);
       if (!cell) { return; }
 
       cell.alive = !newState;
@@ -385,13 +385,13 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
       this.getNeighborKeys(cell).forEach(neighborKey => this.cellsToCheck.add(neighborKey));
     });
 
-    requestAnimationFrame(() => { this.drawCells(); });
+    requestAnimationFrame((): void => { this.drawCells(); });
     this.generationCount -= 1;
   }
 
   private saveCheckpoint(): void {
     this.checkpoint.clear();
-    this.cells.forEach((cell, key) => {
+    this.cells.forEach((cell: Cell, key: string): void => {
       if (cell.alive) {
         this.checkpoint.add(key);
       }
@@ -402,14 +402,14 @@ export class GridComponent implements OnInit, AfterViewInit, OnDestroy {
   returnToCheckpoint(): void {
     this.cells.forEach(cell => cell.alive = false);
     this.checkpoint.forEach(key => {
-      const cell = this.cells.get(key);
+      const cell: Cell | undefined = this.cells.get(key);
       if (cell) {
         cell.alive = true;
         this.cellsToCheck.add(key);
       }
     });
     this.generationCount = this.gameService.getCheckpointGeneration();
-    requestAnimationFrame(() => { this.drawCells(); });
+    requestAnimationFrame((): void => { this.drawCells(); });
   }
 
   ngOnDestroy(): void {
