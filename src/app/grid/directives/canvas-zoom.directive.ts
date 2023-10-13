242dd1ca7386f97a1b66e27f276327ec7439eaa8
leong <leon.goergen@gmail.com>
2023-10-13 12:59:51 +0200

    adding touch to canvas zoom directive

--- src/app/grid/directives/canvas-zoom.directive.ts
+++ src/app/grid/directives/canvas-zoom.directive.ts
@@ -1,7 +1,7 @@
-import {Directive, ElementRef, EventEmitter, HostListener, Input, Output} from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, Output, HostListener } from '@angular/core';
+import { TransformationMatrixService } from '../../services/transformation-matrix.service';
+import { DrawingContext } from '../../models/drawing-context.model';
 import {MAIN_GRID_CONSTANTS} from "../../app.constants";
-import {TransformationMatrixService} from "../../services/transformation-matrix.service";
-import {DrawingContext} from "../../models/drawing-context.model";
 
 @Directive({
   selector: '[appCanvasZoom]'
@@ -10,15 +10,32 @@ export class CanvasZoomDirective {
   @Input() gridSize!: number;
   @Input() transformationMatrix!: any;
   @Input() drawingContext!: DrawingContext;
-  @Output() zoomRequested: EventEmitter<{ x: number, y: number, amount: number }> = new EventEmitter<{ x: number, y: number, amount: number }>();
+  @Output() zoomRequested: EventEmitter<{ x: number; y: number; amount: number }> = new EventEmitter<{
+    x: number;
+    y: number;
+    amount: number;
+  }>();
 
-  constructor(private el: ElementRef,
-              private transformationMatrixService: TransformationMatrixService) {}
+  private initialPinchDistance: number = 0;
+
+  constructor(
+    private el: ElementRef,
+    private transformationMatrixService: TransformationMatrixService
+  ) {}
 
   @HostListener('wheel', ['$event'])
-  handleZoom(event: WheelEvent): void {
+  @HostListener('touchstart', ['$event'])
+  handleZoom(event: WheelEvent | TouchEvent): void {
     event.preventDefault();
 
+    if (event instanceof WheelEvent) {
+      this.handleMouseWheelZoom(event);
+    } else if (event.touches && event.touches.length === 2) {
+      this.handlePinchZoom(event);
+    }
+  }
+
+  private handleMouseWheelZoom(event: WheelEvent): void {
     const rect: DOMRect = (this.el.nativeElement as HTMLElement).getBoundingClientRect();
 
     const x: number = event.clientX - rect.left;
@@ -28,5 +45,40 @@ export class CanvasZoomDirective {
     this.transformationMatrix = this.transformationMatrixService.scaleAt(this.transformationMatrix, this.drawingContext, { x, y }, amount, this.gridSize);
 
     this.zoomRequested.emit({ x, y, amount });
+  }
+
+  private handlePinchZoom(event: TouchEvent): void {
+    const touch1 = event.touches[0];
+    const touch2 = event.touches[1];
+
+    if (!touch1 || !touch2) return;
+
+    const x1: number = touch1.clientX;
+    const y1: number = touch1.clientY;
+    const x2: number = touch2.clientX;
+    const y2: number = touch2.clientY;
+
+    const currentPinchDistance = Math.hypot(x1 - x2, y1 - y2);
+
+    if (this.initialPinchDistance === 0) {
+      this.initialPinchDistance = currentPinchDistance;
+      return;
+    }
+
+    const amount = currentPinchDistance / this.initialPinchDistance;
+
+    this.initialPinchDistance = currentPinchDistance;
+
+    const x = (x1 + x2) / 2;
+    const y = (y1 + y2) / 2;
+
+    this.transformationMatrix = this.transformationMatrixService.scaleAt(this.transformationMatrix, this.drawingContext, { x, y }, amount, this.gridSize);
+
+    this.zoomRequested.emit({ x, y, amount });
+  }
+
+  @HostListener('touchend')
+  resetInitialPinchDistance(): void {
+    this.initialPinchDistance = 0;
   }
 }
